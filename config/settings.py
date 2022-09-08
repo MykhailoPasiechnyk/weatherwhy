c860416e32dc14651e2c2d8e6d007ba328a3458b
MykhailoPasiechnyk <pasiechnyk.ms@gmail.com>
2022-09-08 12:47:49 +0200

    fix path to BASE DIR

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ import os
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
